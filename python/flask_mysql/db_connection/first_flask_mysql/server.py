e8920cb9f2c53c70100549a1ae61be9f516a8263
Jasper Dalawangbayan <jazdal.md@gmail.com>
2023-04-27 08:28:45 +0100

    Updated 'First Flask' project

--- python/flask_mysql/db_connection/first_flask_mysql/server.py
+++ python/flask_mysql/db_connection/first_flask_mysql/server.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, session
 from friend import Friend
 
 app = Flask(__name__)
-app.secret_key = 'keep it secret, keep it safe' # modify your secret key accordingly         
+app.secret_key = 'friends are precious' # modify your secret key accordingly         
 
 # The following sample code blocks need to be created for every route. You may include optional parameters and arguments into the functions as necessary:
 @app.route('/')                 
@@ -14,16 +14,19 @@ def index():
     print(friends)
     return render_template('index.html', all_friends = friends)
 
-# The following code block is included if your project has a form for user input and registration:
-@app.route('/users', methods=['POST'])
-def create_user():
-    print(f'Got post info')
-    print(request.form)
-    session['username'] = request.form['name']
-    session['useremail'] = request.form['email']
-    # NEVER render a template on a POST request
-    # Instead, redirect to either the index or another route:
-    return redirect('/show')
+@app.route('/create_friend', methods=['POST'])
+def create_friend():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"], 
+        "lname": request.form["lname"], 
+        "occ": request.form["occ"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/')
 
 @app.route('/show')
 def show_user():
